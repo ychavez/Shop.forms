9d4172bdbebc475757844d97b0b55f2b6280b4a8
ychavez <you400@gmail.com>
2020-10-26 21:03:08 -0600

    Agregar archivos de proyecto.

---
+++ Shop.forms/Shop.forms.Android/SplashActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Shop.forms.Droid
+{
+    [Activity(Label = "Tienda", MainLauncher = true, Theme = "@style/SplashTheme")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startup = new Task(StartUp);
+            startup.Start();
+        }
+
+
+        async void StartUp()
+        {
+            await Task.Delay(2000);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+        }
+
+    }
+}
---
+++ Shop.forms/Shop.forms/INotification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.forms
+{
+    public interface INotification
+    {
+        void CreateNotification(string title, string message);
+    }
+}
